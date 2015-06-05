5e864900c7b37d7f9e23189c80390fa0786e7f6e
Christos Devaris <c.devaris@ccseducation.com>
2015-06-05 16:22:19 +0300

    Chapter 2

---
+++ navigation/2_9/thisact.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+var currTar = "";
+var objPositions = {};
+var dndCookie = 'ccTAMex2_9';
+var cookieLifetime = 365;
+var thenext = 1;
+var allObjs = 0;
+var totalQs = 4;
+
+
+$(document).ready(function() {
+	// COMPLETED MAIN ACTIVITY 
+	if(window.top){window.top.activateFor();} else {window.parent.activateFor();}
+	
+	// HOME ANIME 
+	if(window.top){window.top.animateHome();} else {window.parent.animateHome();}
+				
+	 positionText(myText);
+});

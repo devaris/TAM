d52b15716d96d0a130016cacb50f587d85a36d93
Christos Devaris <c.devaris@ccseducation.com>
2015-06-05 17:43:12 +0300

    Chapter 4

---
+++ navigation/4_2/data.js
@@ -0,0 +1,19 @@
+var exJSON = {"exercise":
+{ 
+	"rubric":"", 
+	"image":"", 
+	"sound":"", 
+	"URLs":"",
+	"checkButton":"false",
+	"items": [
+	{
+		"image":"", 
+		"sound":"", 
+		"stem":'',
+		"answers":["Every..","Try..", "We..", "Agree..", "Jointly.."],
+		"correct":[0, 2]
+	}
+	],
+  	"myText":''
+}
+}
---
+++ navigation/4_4/data.js
@@ -0,0 +1,19 @@
+var exJSON = {"exercise":
+{ 
+	"rubric":"", 
+	"image":"", 
+	"sound":"", 
+	"URLs":"",
+	"checkButton":"false",
+	"items": [
+	{
+		"image":"", 
+		"sound":"", 
+		"stem":'',
+		"answers":["Make..","Introduce..", "Lead", "Utilize..", "Raise.."],
+		"correct":[1, 2, 3, 4]
+	}
+	],
+  	"myText":''
+}
+}

1ce270f6191b12f90f3a48d0defbd56294c34921
Christos Devaris <c.devaris@ccseducation.com>
2015-06-08 18:44:18 +0300

    Chpater 2 Part II and Chapter 3

---
+++ navigation/2_7/thisact.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+var currTar = "";
+var objPositions = {};
+var dndCookie = 'ccTAMex2_7';
+var cookieLifetime = 365;
+var thenext = 1;
+var allObjs = 0;
+var totalQs = 4;
+
+
+$(document).ready(function() {
+	// COMPLETED MAIN ACTIVITY 
+    if(window.top){window.top.activateFor();} else {window.parent.activateFor();}
+	//positionText(myText);	
+});
